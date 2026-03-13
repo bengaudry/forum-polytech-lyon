0a08c62103938d0ee26b70d892f0ab276a79cab1
bengaudry <bengaudry@outlook.fr>
2026-03-13 22:53:30 +0100

    fix logos

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,17 @@
+import infoLogo from "/images/specialites/info.png"
+import matLogo from '/images/specialites/mat.png'
+import mamLogo from '/images/specialites/mam.png'
+import gbmLogo from '/images/specialites/gbm.png'
+import mecaLogo from '/images/specialites/meca.png'
+import sirLogo from '/images/specialites/sir.png'
+
 export const POLYTECH_SPECIALITIES = [
-  { name: 'INFO', logo: '/images/specialites/info.png' },
-  { name: 'MAT', logo: '/images/specialites/mat.png' },
-  { name: 'MAM', logo: '/images/specialites/mam.png' },
-  { name: 'GBM', logo: '/images/specialites/gbm.png' },
-  { name: 'MECA', logo: '/images/specialites/meca.png' },
-  { name: 'SIR', logo: '/images/specialites/sir.png' },
+  { name: 'INFO', logo: infoLogo },
+  { name: 'MAT', logo: matLogo },
+  { name: 'MAM', logo: mamLogo },
+  { name: 'GBM', logo: gbmLogo },
+  { name: 'MECA', logo: mecaLogo },
+  { name: 'SIR', logo: sirLogo },
 ]
 
 export type SpecialitePolytech = (typeof POLYTECH_SPECIALITIES)[number]['name']
