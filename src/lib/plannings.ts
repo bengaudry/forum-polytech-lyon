edcf0f6960c50e916b13524daa9037f1cdde58a6
bengaudry <bengaudry@outlook.fr>
2026-03-13 22:30:41 +0100

    plannings

---
+++ src/lib/plannings.ts
@@ -0,0 +1,132 @@
+import type { SpecialitePolytech } from './constants'
+
+type Planning = {
+  startHour: Date
+  endHour: Date
+  companies?: { name: string; logo?: string }[]
+  speakers?: { name: string; details?: string }[]
+}
+
+export const plannings: Record<SpecialitePolytech, Array<Planning>> = {
+  MAM: [
+    {
+      startHour: new Date(2026, 3, 26, 9, 0),
+      endHour: new Date(2026, 3, 26, 10, 0),
+      companies: [{ name: 'Thelio', logo: '/images/logos/compressed/thelio.png' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 10, 0),
+      endHour: new Date(2026, 3, 26, 11, 0),
+      companies: [{ name: 'Dattico', logo: '/images/logos/originals/dattico.svg' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 30),
+      endHour: new Date(2026, 3, 26, 12, 30),
+      speakers: [{ name: 'Ambroise Kitohou' }, { name: 'Cheikh Fall', details: 'Promotion 2025' }],
+    },
+  ],
+  INFO: [
+    {
+      startHour: new Date(2026, 3, 26, 9, 0),
+      endHour: new Date(2026, 3, 26, 10, 0),
+      speakers: [{ name: 'Mathieu Mure' }, { name: 'Mohamed Djebali' }, { name: 'Antoine Caron' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 10, 0),
+      endHour: new Date(2026, 3, 26, 11, 0),
+      companies: [{ name: 'Coexya' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 0),
+      endHour: new Date(2026, 3, 26, 11, 30),
+      companies: [{ name: 'Dattico' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 30),
+      endHour: new Date(2026, 3, 26, 12, 30),
+      companies: [{ name: 'Vicinity' }],
+    },
+  ],
+  MAT: [
+    {
+      startHour: new Date(2026, 3, 26, 9, 0),
+      endHour: new Date(2026, 3, 26, 10, 0),
+      companies: [{ name: 'Aero' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 10, 0),
+      endHour: new Date(2026, 3, 26, 11, 0),
+      companies: [{ name: 'Andrice' }, { name: 'Basseti' }, { name: 'Vicinity' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 0),
+      endHour: new Date(2026, 3, 26, 11, 30),
+      speakers: [
+        { name: 'Damien Cabut', details: 'Promotion 2017' },
+        { name: 'Christophe Garnier', details: 'Promotion 1995' },
+      ],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 30),
+      endHour: new Date(2026, 3, 26, 12, 30),
+      companies: [{ name: 'SFEN' }],
+    },
+  ],
+  MECA: [
+    {
+      startHour: new Date(2026, 3, 26, 9, 0),
+      endHour: new Date(2026, 3, 26, 10, 0),
+      companies: [{ name: 'Aero' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 10, 0),
+      endHour: new Date(2026, 3, 26, 11, 0),
+      companies: [{ name: 'Andrice' }, { name: 'Basseti' }, { name: 'Vicinity' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 0),
+      endHour: new Date(2026, 3, 26, 11, 30),
+      speakers: [
+        { name: 'Damien Cabut', details: 'Promotion 2017' },
+        { name: 'Christophe Garnier', details: 'Promotion 1995' },
+      ],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 30),
+      endHour: new Date(2026, 3, 26, 12, 30),
+      companies: [{ name: 'SFEN' }],
+    },
+  ],
+  SIR: [
+    {
+      startHour: new Date(2026, 3, 26, 9, 0),
+      endHour: new Date(2026, 3, 26, 10, 0),
+      companies: [{ name: 'Aero' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 10, 0),
+      endHour: new Date(2026, 3, 26, 11, 0),
+      companies: [{ name: 'Andrice' }, { name: 'Basseti' }, { name: 'Vicinity' }],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 0),
+      endHour: new Date(2026, 3, 26, 11, 30),
+      speakers: [
+        { name: 'Damien Cabut', details: 'Promotion 2017' },
+        { name: 'Christophe Garnier', details: 'Promotion 1995' },
+      ],
+    },
+    {
+      startHour: new Date(2026, 3, 26, 11, 30),
+      endHour: new Date(2026, 3, 26, 12, 30),
+      companies: [{ name: 'SFEN' }],
+    },
+  ],
+  GBM: [
+    {
+      startHour: new Date(2026, 3, 26, 11, 30),
+      endHour: new Date(2026, 3, 26, 12, 30),
+      companies: [{ name: 'Hecapp' }],
+    },
+  ],
+}
--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -1,18 +1,51 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { POLYTECH_SPECIALITIES, type SpecialitePolytech } from '@/lib/constants'
+import { plannings } from '@/lib/plannings'
 
 const selectedSpecialityName = ref<SpecialitePolytech | null>(null)
+
+const speciality = computed(() => {
+  if (!selectedSpecialityName.value) return null;
+  return POLYTECH_SPECIALITIES.find((s) => s.name === selectedSpecialityName.value) || null
+})
+
+const planning = computed(() => {
+  if (!selectedSpecialityName.value) return null
+  return plannings[selectedSpecialityName.value] || null
+})
 </script>
 
 <template>
+  <img v-if="speciality" :src="speciality.logo" :alt="`Logo de la filière ${selectedSpecialityName}`" height="90" class="logo-filiere" />
   <h1>Planning {{ selectedSpecialityName }}</h1>
-  <p v-if="selectedSpecialityName">
+  <div v-if="selectedSpecialityName">
     <button @click="selectedSpecialityName = null" class="back-btn">
       <img src="/images/icons/angle-left.svg" alt="<" width="16" height="16" />
       Revenir
     </button>
-  </p>
+
+    <ul class="planning">
+      <li v-for="event in planning" :key="event.startHour.toDateString()" class="planning-event">
+        <span class="event-time">{{ event.startHour.toLocaleTimeString(undefined, {
+          hour: '2-digit',
+          minute: '2-digit',
+        }) }} - {{ event.endHour.toLocaleTimeString(undefined, {
+          hour: '2-digit',
+          minute: '2-digit',
+        }) }}</span>
+        <div v-if="event.companies">
+        <strong>{{ event.companies.length > 1 ? "Entreprises" : "Entreprise" }} : </strong>
+        {{ event.companies.map((c) => c.name).join(', ') }}
+
+        </div>
+        <div v-if="event.speakers">
+        <strong>{{ event.speakers.length > 1 ? "Intervenants" : "Intervenant" }} : </strong>
+          {{ event.speakers.map((s) => s.name).join(', ') }}
+        </div>
+      </li>
+    </ul>
+  </div>
   <div v-else class="grid">
     <button
       v-for="speciality in POLYTECH_SPECIALITIES"
@@ -29,11 +62,16 @@ const selectedSpecialityName = ref<SpecialitePolytech | null>(null)
 </template>
 
 <style scoped lang="scss">
-.back-btn {
-  img {
-    transform: translateY(0.5px);
-  }
+.logo-filiere {
+  display: block;
+  margin: 0 auto;
+  margin-top: 3rem;
+  margin-bottom: 1rem;
+}
 
+.back-btn {
+  width: 480px;
+  margin: 0 auto;
   background-color: transparent;
   font-size: 1.1rem;
   border: none;
@@ -42,6 +80,44 @@ const selectedSpecialityName = ref<SpecialitePolytech | null>(null)
   align-items: center;
   gap: 0.4rem;
   margin-bottom: 1rem;
+
+  img {
+    transform: translateY(0.5px);
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.event-time {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+.planning {
+  list-style-type: none;
+  max-width: 480px;
+  margin: 0 auto;
+  padding: 0;
+}
+
+.event-companies-list {
+  list-style-type: none;
+  padding-left: 0;
+  margin: 0;
+}
+
+.event-company {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+
+  img {
+    object-fit: contain;
+  }
 }
 
 .grid {
@@ -55,8 +131,16 @@ const selectedSpecialityName = ref<SpecialitePolytech | null>(null)
   margin: 0 auto;
 }
 
+.planning-event {
+  padding: 1.5rem 2rem;
+}
+
+.planning-event:not(:last-child) {
+  border-bottom: 1px solid #e6e6e6;
+}
+
 .speciality-link {
-  background-color: transparent;
+  background-color: #fff;
   border: 1px solid #e6e6e6;
   display: flex;
   flex-grow: 1;
@@ -65,8 +149,7 @@ const selectedSpecialityName = ref<SpecialitePolytech | null>(null)
   align-items: center;
   justify-content: center;
   color: black;
-  min-width: 150px;
-  max-width: 220px;
+  width: 200px;
   margin: 10px;
   height: 150px;
   padding: 20px 40px;
@@ -83,6 +166,11 @@ const selectedSpecialityName = ref<SpecialitePolytech | null>(null)
   span {
     font-weight: bold;
     font-size: 1.2rem;
+  }
+
+  &:hover {
+    cursor: pointer;
+    background-color: #f0f0f0;
   }
 }
 </style>
