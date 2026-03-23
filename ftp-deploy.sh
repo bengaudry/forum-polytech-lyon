74b02d4b344fc0a86ba90e786b6e691fce36337f
bengaudry <bengaudry@outlook.fr>
2026-03-23 19:38:19 +0100

    ci: script for ftp deployment github actions

---
+++ ftp-deploy.sh
@@ -0,0 +1,14 @@
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <ftp_username> <ftp_password>"
+    exit 1
+fi
+
+pnpm build
+
+lftp "ftp://$1:$2@ftp.forum-polytech-lyon.org" < ftp-deploy-instructions.sh
+
+if $? -ne 0; then
+    echo "Error: FTP upload failed."
+    exit 1
+fi
+echo Done!
