2ab65b53a5cfada2a283e37dbc1e2273d5faa253
bengaudry <bengaudry@outlook.fr>
2026-03-23 11:49:00 +0100

    chore: message for plan unavailable

--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -2,6 +2,13 @@
 
 <template>
   <h1>Plan du forum</h1>
+  <p>Le plan n'est pas encore disponible.</p>
 </template>
 
-<style scoped></style>
+<style scoped>
+p {
+  font-size: 1.2em;
+  text-align: center;
+  color: #555;
+}
+</style>
