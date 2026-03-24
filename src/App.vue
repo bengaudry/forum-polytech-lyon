f51e6bd4427057905481066962c8dd7e9d415d4b
bengaudry <bengaudry@outlook.fr>
2026-03-24 17:52:18 +0100

    refactor: removed useless code in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <script setup lang="ts">
-import './styles/common.css'
-import { RouterView, useRouter } from 'vue-router'
 import Navbar from '@/components/common/Navbar.vue'
 import Footer from '@/components/common/Footer.vue'
-
-const { replace } = useRouter()
-
-// Redirection de l'ancienne URL vers la nouvelle si le paramètre "page" est présent et correspond à une page valide
-// (pour gérer les problèmes liés a la SPA vue router)
-try {
-  const currentUrl = new URL(window.location.href)
-  const redirectPage = currentUrl.searchParams.get('page')
-  if (redirectPage && ['planning', 'plan', 'stands'].includes(redirectPage.toString())) {
-    console.warn("Redirection de l'ancienne URL vers la nouvelle")
-    replace(`/${redirectPage.toString()}`)
-  }
-} catch (_) {
-  console.warn(
-    "Impossible d'analyser l'URL actuelle pour la redirection, aucune action de redirection effectuée",
-  )
-}
+import './styles/common.css'
 </script>
 
 <template>
